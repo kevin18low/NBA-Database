1f37aa654c4cf429f231036c7cdc372a782d53d5
Kevin <klow05@student.ubc.ca>
2024-09-19 23:25:26 -0700

    Added image search bar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,24 +2,41 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 function App() {
-    const [img, setImg] = useState(null);
+    const defaultImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png"
+    const [img, setImg] = useState(defaultImg);
+    const [input, setInput] = useState("");
+    const [search, setSearch] = useState(false);
 
     useEffect(() => {
-        const getImageData = async () => {
-            const name = "Lebron"
-            try {
-                const response = await axios(`/api/image?playerName=${encodeURIComponent(name)}`);
-                const data = response.data;
-                setImg(data.image);
-            } catch (error) {
-                console.log("Frontend error");
-            }              
-        }
-        getImageData();
-    });
+        if (search) {
+            const getImageData = async () => {
+                const name = input;
+                try {
+                    const response = await axios(`/api/image?playerName=${encodeURIComponent(name)}`);
+                    const data = response.data;
+                    setImg(data.image);
+                } catch (error) {
+                    console.log("Frontend error");
+                }              
+            }
+            getImageData();
+            setSearch(false);
+        }        
+    }, [search, input]);
+
+    function handleChange(event) {
+        setInput(event.target.value);
+    }
+
+    function handleClick() {
+        setSearch(true);
+    }
 
     return <div>
         <img src={img} alt="basketball player"></img>
+        <input type="text" placeholder="Type here" onChange={handleChange}/>
+        <button onClick={handleClick}>Search</button>
+        <h3>{input}</h3>
     </div>;
 }
 
