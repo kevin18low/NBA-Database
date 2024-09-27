6757d295e998ad8f93d2a77e643cbd0eb3b88d02
Kevin <klow05@student.ubc.ca>
2024-09-26 21:04:58 -0700

    Discarding files

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,6 @@ app.use(express.static("../frontend/public"));
 app.get("/api/image", async (req, res) => {
     const googleKey = process.env.GOOGLE_API_KEY;
     const cx = process.env.SEARCH_ENGINE_ID;
-    const nbaKey = process.env.NBA_API_KEY;
     const player = req.query.playerName;
     const imgSearch = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(`${player} playing basketball`)}&cx=${cx}&key=${googleKey}&searchType=image`;
 
@@ -30,6 +29,26 @@ app.get("/api/image", async (req, res) => {
         console.log(error);
     }
 });
+
+app.get("/api/playerInfo", async (req, res) => {
+    const url = "https://api.balldontlie.io/v1";
+    const nbaKey = process.env.NBA_API_KEY;
+    try {
+        const result = await axios(url, {
+            headers: {
+                Authorization: nbaKey
+            },
+            params: {
+                q: req.query.playerName
+            }
+        })
+
+        const playerInfo = result.data.data;
+        console.log(playerInfo);
+    } catch (error) {
+        console.log("Error getting from nba api");
+    }
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,22 +4,33 @@ import axios from "axios";
 function App() {
     const defaultImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png"
     const [img, setImg] = useState(defaultImg);
+    const [info, setInfo] = useState([]);
     const [input, setInput] = useState("");
     const [search, setSearch] = useState(false);
 
     useEffect(() => {
         if (search) {
+            const name = input;
             const getImageData = async () => {
-                const name = input;
                 try {
                     const response = await axios(`/api/image?playerName=${encodeURIComponent(name)}`);
                     const data = response.data;
                     setImg(data.image);
                 } catch (error) {
-                    console.log("Frontend error");
+                    console.log("Frontend search error");
                 }              
             }
             getImageData();
+
+            const getPlayerData = async () => {
+                try {
+                    const response = await axios(`/api/playerInfo?playerName=${encodeURIComponent(name)}`)
+                    const data = response.data;
+                    setInfo(data);
+                } catch (error) {
+                    console.log("Frontend nba error");
+                }
+            }
             setSearch(false);
         }        
     }, [search, input]);
@@ -34,6 +45,7 @@ function App() {
 
     return <div>
         <img src={img} alt="basketball player"></img>
+        <h3>{info}</h3>
         <input type="text" placeholder="Type here" onChange={handleChange}/>
         <button onClick={handleClick}>Search</button>
         <h3>{input}</h3>
